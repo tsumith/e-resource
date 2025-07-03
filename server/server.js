1f11dc2679c892ae0905194ac86aa5680a7a0379
tsumith <tsumith987@gmail.com>
2025-07-03 16:12:00 +0530

    modified package.json

--- server/server.js
+++ server/server.js
@@ -16,6 +16,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/resources', resourceRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+// });
+module.exports = (req, res) => app(req, res);
