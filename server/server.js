8af475f5f96d866d95634c7c55fc88cc4ed3c8ca
tsumith <tsumith987@gmail.com>
2025-07-03 18:15:12 +0530

    solved cors issue

--- server/server.js
+++ server/server.js
@@ -10,7 +10,13 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+const allowedOrigin = process.env.FRONTEND_URI || 'http://localhost:3000';
+
+app.use(cors({
+    origin: allowedOrigin,
+    credentials: true,
+}));
+
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/resources', resourceRoutes);
